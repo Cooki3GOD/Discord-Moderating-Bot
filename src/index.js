ae5cc3d9938fabdf188f4d58f1b275cf01bb969b
Lukasz Szczygielski <l_szczygielski@o2.pl>
2025-03-11 13:47:59 +0100

    Add tempban command

--- src/index.js
+++ src/index.js
@@ -18,59 +18,102 @@ client.once('ready', () => {
 });
 
 client.on("messageCreate", async (message) => {
-    console.log(message);
+    if (message.author.bot) return;
 
-    if (!message.author.bot) {
-        const args = message.content.split(' ');
-        const command = args.shift().toLowerCase();
+    const args = message.content.split(' ');
+    const command = args.shift().toLowerCase();
 
-        if (command === '!help') {
-            message.channel.send('Available commands:\n!kick @user - Kick a user\n!ban @user - Ban a user');
-        }
-
-        if (command === '!kick') {
-            if (!message.member.permissions.has('KICK_MEMBERS')) {
-                return message.reply('You do not have permission to kick members.');
-            }
-
-            const member = message.mentions.members.first();
-            if (member) {
-                try {
-                    await member.kick();
-                    message.channel.send(`${member.user.tag} has been kicked.`);
-                } catch (error) {
-                    message.channel.send(`I cannot kick ${member.user.tag}.`);
-                    console.error(error);
-                }
-            } else {
-                message.reply('Please mention a valid member to kick.');
-            }
-        }
-
-        if (command === '!ban') {
-            if (!message.member.permissions.has('BAN_MEMBERS')) {
-                return message.reply('You do not have permission to ban members.');
-            }
-
-            const member = message.mentions.members.first();
-            if (member) {
-                try {
-                    await member.ban();
-                    message.channel.send(`${member.user.tag} has been banned.`);
-                } catch (error) {
-                    message.channel.send(`I cannot ban ${member.user.tag}.`);
-                    console.error(error);
-                }
-            } else {
-                message.reply('Please mention a valid member to ban.');
-            }
-        }
-
-        // Commented out the DM sending part
-        // try {
-        //     await message.author.send(`Echo ${message.content}`);
-        // } catch (error) {
-        //     console.error(`Could not send message to ${message.author.tag}:`, error);
-        // }
+    switch (command) {
+        case '!help':
+            handleHelpCommand(message);
+            break;
+        case '!kick':
+            handleKickCommand(message);
+            break;
+        case '!ban':
+            handleBanCommand(message);
+            break;
+        case '!tempban':
+            handleTempBanCommand(message, args);
+            break;
+        default:
+            break;
     }
 });
+
+function handleHelpCommand(message) {
+    message.channel.send(
+        'Available commands:\n' +
+        '!kick @user - Kick a user\n' +
+        '!ban @user - Ban a user\n' +
+        '!tempban @user duration - Temporarily ban a user for a specified duration (in minutes)'
+    );
+}
+
+async function handleKickCommand(message) {
+    if (!message.member.permissions.has('KICK_MEMBERS')) {
+        return message.reply('You do not have permission to kick members.');
+    }
+
+    const member = message.mentions.members.first();
+    if (!member) {
+        return message.reply('Please mention a valid member to kick.');
+    }
+
+    try {
+        await member.kick();
+        message.channel.send(`${member.user.tag} has been kicked.`);
+    } catch (error) {
+        message.channel.send(`I cannot kick ${member.user.tag}.`);
+        console.error(error);
+    }
+}
+
+async function handleBanCommand(message) {
+    if (!message.member.permissions.has('BAN_MEMBERS')) {
+        return message.reply('You do not have permission to ban members.');
+    }
+
+    const member = message.mentions.members.first();
+    if (!member) {
+        return message.reply('Please mention a valid member to ban.');
+    }
+
+    try {
+        await member.ban();
+        message.channel.send(`${member.user.tag} has been banned.`);
+    } catch (error) {
+        message.channel.send(`I cannot ban ${member.user.tag}.`);
+        console.error(error);
+    }
+}
+
+async function handleTempBanCommand(message, args) {
+    if (!message.member.permissions.has('BAN_MEMBERS')) {
+        return message.reply('You do not have permission to ban members.');
+    }
+
+    const member = message.mentions.members.first();
+    const duration = parseInt(args[0], 10);
+
+    if (!member || isNaN(duration)) {
+        return message.reply('Please mention a valid member to ban and specify a valid duration in minutes.');
+    }
+
+    try {
+        await member.ban();
+        message.channel.send(`${member.user.tag} has been temporarily banned for ${duration} minutes.`);
+
+        setTimeout(async () => {
+            try {
+                await message.guild.members.unban(member.id);
+                message.channel.send(`${member.user.tag} has been unbanned.`);
+            } catch (error) {
+                console.error(`Failed to unban ${member.user.tag}:`, error);
+            }
+        }, duration * 60 * 1000); // Convert minutes to milliseconds
+    } catch (error) {
+        message.channel.send(`I cannot ban ${member.user.tag}.`);
+        console.error(error);
+    }
+}
